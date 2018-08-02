6e8e2edeb5b4fc4180e0cab03cb14402fa04083a
YinTianliang <bailong104@gmail.com>
2018-08-02 13:00:11 +0800

    add comments

--- utils/logging.zsh
+++ utils/logging.zsh
@@ -1,5 +1,13 @@
 #!/usr/bin/env zsh
 
+# Usage:
+# 
+# foo () {
+#     logging "fooing"
+#     SOME_CODE
+# }
+# foo 3>/dev/stdout &>/tmp/kili-manjaro.log
+# 这样只会在屏幕上输出 "fooing", 其他内容连同 "fooing" 在内会被重定向到 .log 文件中
 logging () {
     print "[$funcstack[2]] $*" 1>&3
     print "[$funcstack[2]] $*"
